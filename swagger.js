757cc87fb2df9aab5ff841c3f59484c8fd9b2c0c
RhodaMabs <mab21004@byui.edu>
2022-11-19 22:36:51 +0200

    commit

--- swagger.js
+++ swagger.js
@@ -5,8 +5,10 @@ const doc = {
     title: 'My API',
     description: 'Contacts API'
   },
-  host: 'mabundur341.onrender.com',
-  schemes: ['https']
+  //host: 'mabundur341.onrender.com',
+  //schemes: ['https']
+  host: 'localhost:8080',
+  schemes: ['http']
 };
 
 const outputFile = './swagger.json';
